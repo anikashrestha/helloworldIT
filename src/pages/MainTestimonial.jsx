aaeb7518e1c20de9321b2bf31268936f61326123
anikashrestha <shtanika409@gmail.com>
2024-03-07 16:09:26 +0545

    first

--- src/pages/MainTestimonial.jsx
+++ src/pages/MainTestimonial.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
 import './MainTestimonial.css';
+import { useInView } from 'react-intersection-observer';
 
-export default function MainTestimonials() {
+
+const FadeInSection = ({ children }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <div
+      ref={ref}
+      className={`fade-in-section ${inView ? 'is-visible' : ''}`}
+    >
+      {children}
+    </div>
+  );
+};
+const MainTestimonials = () => {
     return(
         <div className='section-c'>
         <div className='group-10' />
         <div className='group-11'>
+          <FadeInSection>
           <div className='img-7' />
           <span className='text-20'>
             “Great service and on time deliveries with exceptional servers.”
           </span>
           <span className='text-21'>Rohit Kumar Shrestha</span>
           <span className='text-22'>Founder at Distar</span>
+          </FadeInSection>
         </div>
         <div className='box-9'>
+          <FadeInSection>
           <div className='pic-b' />
           <span className='text-23'>
             “I could not be more thrilled that I ended up deciding on Circle to
@@ -21,8 +41,10 @@ export default function MainTestimonials() {
           </span>
           <span className='text-24'>Rohit Kumar Shrestha</span>
           <span className='text-25'>Founder at Kimatsu</span>
+          </FadeInSection>
         </div>
         <div className='group-12'>
+          <FadeInSection>
           <div className='img-8' />
           <span className='text-26'>
             “Yet preference connection unpleasant yet melancholy but end
@@ -30,8 +52,10 @@ export default function MainTestimonials() {
           </span>
           <span className='text-27'>Rohit Kumar Shrestha</span>
           <span className='text-28'>Founder at Google</span>
+          </FadeInSection>
         </div>
         {/* <div className='slider' /> */}
       </div>
     );
 }
+export default MainTestimonials;
