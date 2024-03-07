d1419e929c2db5437655479ff9be70a6202f8150
anikashrestha <shtanika409@gmail.com>
2024-03-07 16:08:30 +0545

    first

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,8 +6,7 @@ const FadeInSection = ({ children }) => {
     const { ref, inView } = useInView({
       triggerOnce: true,
       threshold: 0.1,
-    });
-  
+    });  
     return (
       <div
         ref={ref}
@@ -20,12 +19,14 @@ const FadeInSection = ({ children }) => {
   const About = () => {
     return(
         // <div className='main-container'>
+        
             <div className='group-2'>
                 <div className='pic-2' />
                 <div className='section-2'>
                 
                     <div className='section-3'>
                     <span className='textAbout-1'>Web Application Development</span>
+                    <FadeInSection>
                     <div className='box-1'>
                         <span className='textAbout-2'>Empowering </span>
                         <span className='textAbout-3'>Healthcare </span>
@@ -36,10 +37,13 @@ const FadeInSection = ({ children }) => {
                         Advanced Information Solutions
                         </span>
                     </div>
+                    </FadeInSection>
+                    <FadeInSection>
                     <span className='textAbout-6'>
                         Revolutionizing Health Information Management and Web-based
                         Software Solutions for Excellence in Healthcare.
                     </span>
+                    </FadeInSection>
                     </div>
                     <div className='group-3'>
                     <div className='insta' />
@@ -49,12 +53,16 @@ const FadeInSection = ({ children }) => {
                 </div>
                 
                 <div className='wrapper-3'>
+                <FadeInSection>
                     <div className='logo' />
-                    {/* <div id="introSVGContainer" ></div> */}
+                    
+                    </FadeInSection>
                 </div>
                 
+                
             </div>
-        // </div>
+            
+        
     );
 }
   
--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,28 +1,54 @@
 import React from 'react';
 import './Clients.css';
+import { useInView } from 'react-intersection-observer';
 
-export default function Clients() {
+const FadeInSection = ({ children }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  return (
+    <div
+      ref={ref}
+      className={`fade-in-section ${inView ? 'is-visible' : ''}`}
+    >
+      {children}
+    </div>
+  );
+};
+const Clients = () =>  {
     return(
         <div className='group-4'>
-        <span className='text-a'>Our Clients</span>
+        <FadeInSection>
+          <span className='text-a'>Our Clients</span>
+        </FadeInSection>
         <div className='group-5'>
+          
           <div className='wrapper-4'>
+          <FadeInSection>
             <div className='box-4'>
               <div className='img-2' />
               <div className='img-3' />
-            </div>
+            </div>            
             <span className='text-b'>Information Management System</span>
+            </FadeInSection>
           </div>
+          
           <div className='wrapper-5' />
+          
           <div className='box-5'>
+          <FadeInSection>
             <div className='section-4'>
               <div className='img-4' />
               <div className='pic-6' />
             </div>
             <span className='text-b'>Home Appliances</span>
+            </FadeInSection>
           </div>
           <div className='section-5' />
           <div className='section-6'>
+            <FadeInSection>
             <div className='wrapper-6'>
               <div className='pic-7' />
               <div className='img-5' />
@@ -30,8 +56,10 @@ export default function Clients() {
               {/* <div className='pic-9' /> */}
             </div>
             <span className='text-b'>Others</span>
+            </FadeInSection>
           </div>
         </div>
       </div>
     );
 }
+export default Clients;
